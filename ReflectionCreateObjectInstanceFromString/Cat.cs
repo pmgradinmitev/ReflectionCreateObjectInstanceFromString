56190e99ea74855d0e5eb7b959b3e93c1a712f7f
pmgradinmitev <pmg.tadinmitev92@gmail.com>
2025-01-25 22:23:52 +0200

    Add project files.

---
+++ ReflectionCreateObjectInstanceFromString/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionCreateObjectInstanceFromString
+{
+    internal class Cat
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Meowwwwww!");
+        }
+    }
+}
---
+++ ReflectionCreateObjectInstanceFromString/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ReflectionCreateObjectInstanceFromString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fully qualified name of the class including the namespace
+            string className = "ReflectionCreateObjectInstanceFromString.Cat";
+
+            // Get the type of the class
+            Type type = Type.GetType(className);
+            if (type == null)
+            {
+                Console.WriteLine($"Class '{className}' not found.");
+                return;
+            }
+
+            // Create an instance of the class
+            object instance = Activator.CreateInstance(type);
+            if (instance is Cat myClassInstance)
+            {
+                // Call a method on the created instance (optional)
+                myClassInstance.Sound();
+            }
+        }
+    }
+}
